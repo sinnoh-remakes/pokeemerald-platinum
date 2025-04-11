f3e5b87aaa13947e57d3cc888d87b2c6d57d5cd8
blloop <42186044+blloop@users.noreply.github.com>
2025-04-10 21:38:10 -0700

    add maps up to jubilife

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -577,7 +577,9 @@ gStdScripts_End::
 	.include "data/maps/Route119_WeatherInstitute_2F/scripts.inc"
 	.include "data/maps/Route119_House/scripts.inc"
 	.include "data/maps/Route124_DivingTreasureHuntersHouse/scripts.inc"
-
+	.include "data/maps/LittlerootTown_OtherHouseA/scripts.inc"
+	.include "data/maps/LittlerootTown_OtherHouseB/scripts.inc"
+	
 	.include "data/scripts/std_msgbox.inc"
 	.include "data/scripts/trainer_battle.inc"
 	.include "data/scripts/new_game.inc"
