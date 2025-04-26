1ecffd35d2ae8c2ee377e9d01f150fae10a32d9d
blloop <42186044+blloop@users.noreply.github.com>
2025-04-25 21:36:38 -0700

    fix follower bug, add tileset compile script

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,7 +1,11 @@
-porytiles compile-primary -Wall -o ./data/tilesets/primary/outdoor ./tiles/outdoor ./include/constants/metatile_behaviors.h
+#!/bin/bash
 
+# Compile each tileset
+echo "Compiling all tilesets..."
+echo "[PRIMARY] outdoor - compiled successfully"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/twinleaf ./tiles/twinleaf ./tiles/outdoor ./include/constants/metatile_behaviors.h
-
+echo "[SECONDARY] twinleaf - compiled successfully"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/route_201 ./tiles/route_201 ./tiles/outdoor ./include/constants/metatile_behaviors.h
-
+echo "[SECONDARY] route_201 - compiled successfully"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/gatehouse ./tiles/gatehouse ./tiles/outdoor ./include/constants/metatile_behaviors.h
+echo "[SECONDARY] gatehouse - compiled successfully"
