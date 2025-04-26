529cc97fe64686376d6e2986b3d4262df4c874df
blloop <42186044+blloop@users.noreply.github.com>
2025-04-25 20:06:08 -0700

    fix starter event graphic glitch

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -845,11 +845,6 @@ void ChooseStarter(void)
     gMain.savedCallback = CB2_GiveStarter;
 }
 
-// void StartFirstBattle(void)
-// {
-//     SetMainCallback2(CB2_StartFirstBattle);
-// }
-
 static void CB2_GiveStarter(void)
 {
     u16 starterMon;
@@ -858,11 +853,10 @@ static void CB2_GiveStarter(void)
     starterMon = GetStarterPokemon(gSpecialVar_Result);
     ScriptGiveMon(starterMon, 5, ITEM_NONE);
     ResetTasks();
-    SetMainCallback2(CB2_EndFirstBattle);
-    // FODO: Fix graphic flash after removing battle
+    SetMainCallback2(CB2_StartFirstBattle);
     // PlayBattleBGM();
-    // SetMainCallback2(CB2_StartFirstBattle);
-    // BattleTransition_Start(B_TRANSITION_BLUR);
+    BattleTransition_Start(B_TRANSITION_BLUR);
+    BeginNormalPaletteFade(0xFFFFFFFF, -1, 0, 0x10, 0);
 }
 
 static void CB2_StartFirstBattle(void)
@@ -870,12 +864,12 @@ static void CB2_StartFirstBattle(void)
     UpdatePaletteFade();
     RunTasks();
 
-    if (IsBattleTransitionDone() == TRUE)
+    if (!gPaletteFade.active)
     {
         gBattleTypeFlags = BATTLE_TYPE_FIRST_BATTLE;
         gMain.savedCallback = CB2_EndFirstBattle;
         FreeAllWindowBuffers();
-        SetMainCallback2(CB2_InitBattle);
+        SetMainCallback2(CB2_EndFirstBattle);
         RestartWildEncounterImmunitySteps();
         ClearPoisonStepCounter();
         IncrementGameStat(GAME_STAT_TOTAL_BATTLES);
