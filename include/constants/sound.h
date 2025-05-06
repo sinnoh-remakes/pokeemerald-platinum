44ae989223504d021c9a2ca2a7f994bb18fbf8be
blloop <42186044+blloop@users.noreply.github.com>
2025-05-05 21:22:04 -0700

    Release DPPt Music Expansion via https://github.com/CyanSMP64/pokeemerald/tree/dppt_music

--- include/constants/sound.h
+++ include/constants/sound.h
@@ -1,24 +1,46 @@
 #ifndef GUARD_CONSTANTS_SOUND_H
 #define GUARD_CONSTANTS_SOUND_H
 
-#define FANFARE_LEVEL_UP             0
-#define FANFARE_OBTAIN_ITEM          1
-#define FANFARE_EVOLVED              2
-#define FANFARE_OBTAIN_TMHM          3
-#define FANFARE_HEAL                 4
-#define FANFARE_OBTAIN_BADGE         5
-#define FANFARE_MOVE_DELETED         6
-#define FANFARE_OBTAIN_BERRY         7
-#define FANFARE_AWAKEN_LEGEND        8
-#define FANFARE_SLOTS_JACKPOT        9
-#define FANFARE_SLOTS_WIN           10
-#define FANFARE_TOO_BAD             11
-#define FANFARE_RG_POKE_FLUTE       12
-#define FANFARE_RG_OBTAIN_KEY_ITEM  13
-#define FANFARE_RG_DEX_RATING       14
-#define FANFARE_OBTAIN_B_POINTS     15
-#define FANFARE_OBTAIN_SYMBOL       16
-#define FANFARE_REGISTER_MATCH_CALL 17
+#define FANFARE_LEVEL_UP                 0
+#define FANFARE_HEAL                     1
+#define FANFARE_OBTAIN_BADGE             2
+#define FANFARE_OBTAIN_ITEM              3
+#define FANFARE_EVOLVED                  4
+#define FANFARE_OBTAIN_TMHM              5
+#define FANFARE_EVOLUTION_INTRO          6
+#define FANFARE_MOVE_DELETED             7
+#define FANFARE_OBTAIN_BERRY             8
+#define FANFARE_AWAKEN_LEGEND            9
+#define FANFARE_SLOTS_JACKPOT            10
+#define FANFARE_SLOTS_WIN                11
+#define FANFARE_TOO_BAD                  12
+#define FANFARE_RG_JIGGLYPUFF            13
+#define FANFARE_RG_DEX_RATING            14
+#define FANFARE_RG_OBTAIN_KEY_ITEM       15
+#define FANFARE_RG_CAUGHT_INTRO          16
+#define FANFARE_RG_PHOTO                 17
+#define FANFARE_RG_POKE_FLUTE            18
+#define FANFARE_OBTAIN_B_POINTS          19
+#define FANFARE_REGISTER_MATCH_CALL      20
+#define FANFARE_OBTAIN_SYMBOL            21
+#define FANFARE_DP_TV_END                22
+#define FANFARE_DP_OBTAIN_ITEM           23
+#define FANFARE_DP_HEAL                  24
+#define FANFARE_DP_OBTAIN_KEY_ITEM       25
+#define FANFARE_DP_OBTAIN_TMHM           26
+#define FANFARE_DP_OBTAIN_BADGE          27
+#define FANFARE_DP_LEVEL_UP              28
+#define FANFARE_DP_OBTAIN_BERRY          29
+#define FANFARE_DP_PARTNER               30
+#define FANFARE_DP_EVOLVED               31
+#define FANFARE_DP_POKETCH               32
+#define FANFARE_DP_MOVE_DELETED          33
+#define FANFARE_DP_ACCESSORY             34
+#define FANFARE_PL_TV_END                35
+#define FANFARE_PL_CLEAR_MINIGAME        36
+#define FANFARE_PL_OBTAIN_ARCADE_POINTS  37
+#define FANFARE_PL_OBTAIN_CASTLE_POINTS  38
+#define FANFARE_PL_OBTAIN_B_POINTS       39
 
 #define CRY_MODE_NORMAL        0 // Default
 #define CRY_MODE_DOUBLES       1 // Shortened cry for double battles
--- src/pokemon.c
+++ src/pokemon.c
@@ -60,6 +60,7 @@
 #include "constants/moves.h"
 #include "constants/regions.h"
 #include "constants/songs.h"
+#include "constants/species.h"
 #include "constants/trainers.h"
 #include "constants/union_room.h"
 #include "constants/weather.h"
@@ -5792,7 +5793,86 @@ u16 GetBattleBGM(void)
     }
     else
     {
-        return MUS_VS_WILD;
+        switch (GetMonData(&gEnemyParty[0], MON_DATA_SPECIES, NULL))
+        {
+        case SPECIES_ARTICUNO:
+        case SPECIES_ZAPDOS:
+        case SPECIES_MOLTRES:
+        case SPECIES_RAIKOU:
+        case SPECIES_ENTEI:
+        case SPECIES_SUICUNE:
+        case SPECIES_LUGIA:
+        case SPECIES_HO_OH:
+            return MUS_RG_VS_LEGEND;
+        case SPECIES_MEWTWO:
+            return MUS_RG_VS_MEWTWO;
+        case SPECIES_MEW:
+            return MUS_VS_MEW;
+        case SPECIES_REGIROCK:
+        case SPECIES_REGICE:
+        case SPECIES_REGISTEEL:
+        case SPECIES_REGIGIGAS:
+            return MUS_VS_REGI;
+        case SPECIES_LATIAS:
+        case SPECIES_LATIOS:
+            return MUS_VS_WILD;
+        case SPECIES_GROUDON:
+        case SPECIES_KYOGRE:
+        case SPECIES_RAYQUAZA:
+            return MUS_VS_KYOGRE_GROUDON;
+        case SPECIES_JIRACHI:
+            return MUS_VS_WILD;
+        case SPECIES_DEOXYS:
+        case SPECIES_DEOXYS_ATTACK:
+        case SPECIES_DEOXYS_DEFENSE:
+        case SPECIES_DEOXYS_SPEED:
+            return MUS_RG_VS_DEOXYS;
+        case SPECIES_UXIE:
+        case SPECIES_MESPRIT:
+        case SPECIES_AZELF:
+            return MUS_DP_VS_UXIE_MESPRIT_AZELF;
+        case SPECIES_DIALGA:
+        case SPECIES_PALKIA:
+            return MUS_DP_VS_DIALGA_PALKIA;
+        case SPECIES_ROTOM:
+        case SPECIES_ROTOM_HEAT:
+        case SPECIES_ROTOM_WASH:
+        case SPECIES_ROTOM_FROST:
+        case SPECIES_ROTOM_FAN:
+        case SPECIES_ROTOM_MOW:
+        case SPECIES_HEATRAN:
+        case SPECIES_MANAPHY:
+        case SPECIES_DARKRAI:
+            return MUS_DP_VS_LEGEND;
+        case SPECIES_GIRATINA:
+        case SPECIES_GIRATINA_ORIGIN:
+            return MUS_PL_VS_GIRATINA;
+        case SPECIES_CRESSELIA:
+        case SPECIES_PHIONE:
+        case SPECIES_SHAYMIN:
+        case SPECIES_SHAYMIN_SKY:
+            return MUS_DP_VS_WILD;
+        case SPECIES_ARCEUS:
+        case SPECIES_ARCEUS_FIGHTING:
+        case SPECIES_ARCEUS_FLYING:
+        case SPECIES_ARCEUS_POISON:
+        case SPECIES_ARCEUS_GROUND:
+        case SPECIES_ARCEUS_ROCK:
+        case SPECIES_ARCEUS_BUG:
+        case SPECIES_ARCEUS_GHOST:
+        case SPECIES_ARCEUS_STEEL:
+        case SPECIES_ARCEUS_FIRE:
+        case SPECIES_ARCEUS_WATER:
+        case SPECIES_ARCEUS_GRASS:
+        case SPECIES_ARCEUS_ELECTRIC:
+        case SPECIES_ARCEUS_PSYCHIC:
+        case SPECIES_ARCEUS_ICE:
+        case SPECIES_ARCEUS_DRAGON:
+        case SPECIES_ARCEUS_DARK:
+            return MUS_DP_VS_ARCEUS;
+        default:
+            return MUS_VS_WILD;
+        }
     }
 }
 
