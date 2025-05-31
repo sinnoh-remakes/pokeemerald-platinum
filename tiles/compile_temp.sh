bca139f98f60b606c7489122679d12f539f52a32
blloop <42186044+blloop@users.noreply.github.com>
2025-05-30 23:35:52 -0700

    add remaining tiles to outdoor_jubilife (sandgem not updated)

--- tiles/compile_temp.sh
+++ tiles/compile_temp.sh
@@ -1,2 +1,2 @@
-echo "Compiling [secondary] twinleaf"
-porytiles compile-secondary -Wall -o ./data/tilesets/secondary/twinleaf ./tiles/twinleaf ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] sandgem"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/sandgem ./tiles/sandgem ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
