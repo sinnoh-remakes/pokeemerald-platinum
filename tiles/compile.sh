53ca94db27cdc2a549e1f87b3f024ee193b9bc04
blloop <42186044+blloop@users.noreply.github.com>
2025-06-29 18:37:30 -0700

    add center and mart to jubilife tiles

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "Compiling [secondary] twinleaf"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/twinleaf ./tiles/twinleaf ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] sandgem"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/sandgem ./tiles/sandgem ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
 echo "Compiling [secondary] jubilife"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/jubilife ./tiles/jubilife ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
