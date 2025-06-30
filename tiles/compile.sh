4ff8a24558c4715fe10b5986dd55160a102a14cc
blloop <42186044+blloop@users.noreply.github.com>
2025-06-29 20:40:47 -0700

    add brown building tiles and decorations

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "Compiling [secondary] twinleaf"
-porytiles compile-secondary -Wall -o ./data/tilesets/secondary/twinleaf ./tiles/twinleaf ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
-echo "Compiling [secondary] sandgem"
-porytiles compile-secondary -Wall -o ./data/tilesets/secondary/sandgem ./tiles/sandgem ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
+echo "Compiling [PRIMARY] outdoor_jubilife"
+porytiles compile-primary -Wall -o ./data/tilesets/primary/outdoor_jubilife ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
 echo "Compiling [secondary] jubilife"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/jubilife ./tiles/jubilife ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
