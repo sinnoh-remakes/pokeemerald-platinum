64821035e7f0dfd1d452e63a2add38ed7f1aa6fe
blloop <42186044+blloop@users.noreply.github.com>
2025-07-01 23:47:24 -0700

    add jubilife decoration tiles

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "Compiling [PRIMARY] outdoor_jubilife"
-porytiles compile-primary -Wall -o ./data/tilesets/primary/outdoor_jubilife ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
 echo "Compiling [secondary] jubilife"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/jubilife ./tiles/jubilife ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
