aa44435789a61ebb5d9e6ca4554638cddce79a33
blloop <42186044+blloop@users.noreply.github.com>
2025-07-20 10:13:01 -0700

    add base route203 tileset with signs

--- src/data/tilesets/headers.h
+++ src/data/tilesets/headers.h
@@ -1006,3 +1006,14 @@ const struct Tileset gTileset_Jubilife =
     .metatileAttributes = gMetatileAttributes_Jubilife,
     .callback = NULL,
 };
+
+const struct Tileset gTileset_Route203 =
+{
+    .isCompressed = TRUE,
+    .isSecondary = TRUE,
+    .tiles = gTilesetTiles_Route203,
+    .palettes = gTilesetPalettes_Route203,
+    .metatiles = gMetatiles_Route203,
+    .metatileAttributes = gMetatileAttributes_Route203,
+    .callback = NULL,
+};
