1e8a96585b8273e99ae867a4645f99db7392697c
blloop <42186044+blloop@users.noreply.github.com>
2025-08-02 15:43:37 -0700

    add dawn walk run sprites

--- src/data/object_events/object_event_graphics.h
+++ src/data/object_events/object_event_graphics.h
@@ -6,6 +6,8 @@ const u32 gObjectEventPic_Johanna[] = INCBIN_U32("graphics/object_events/pics/pe
 const u16 gObjectEventPal_Johanna[] = INCBIN_U16("graphics/object_events/palettes/johanna.gbapal");
 const u32 gObjectEventPic_LucasNormalRunning[] = INCBIN_U32("graphics/object_events/pics/people/lucas/walking.4bpp", "graphics/object_events/pics/people/lucas/running.4bpp");
 const u16 gObjectEventPal_Lucas[] = INCBIN_U16("graphics/object_events/palettes/lucas.gbapal");
+const u32 gObjectEventPic_DawnNormalRunning[] = INCBIN_U32("graphics/object_events/pics/people/dawn/walking.4bpp", "graphics/object_events/pics/people/dawn/running.4bpp");
+const u16 gObjectEventPal_Dawn[] = INCBIN_U16("graphics/object_events/palettes/dawn.gbapal");
 
 const u32 gObjectEventPic_BrendanNormalRunning[] = INCBIN_U32("graphics/object_events/pics/people/brendan/walking.4bpp", "graphics/object_events/pics/people/brendan/running.4bpp");
 const u16 gObjectEventPal_Brendan[] = INCBIN_U16("graphics/object_events/palettes/brendan.gbapal");
