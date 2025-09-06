f949e67e38b2048cad6bef5bf53e5749d4f08350
blloop <42186044+blloop@users.noreply.github.com>
2025-09-06 14:59:18 -0700

    rework outdoor jubilife tileset to support all transitions from jubilife

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Compiling [secondary] jubilife"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/jubilife ./tiles/jubilife ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
 echo "Compiling [PRIMARY] cave_coronet"
 porytiles compile-primary -Wall -o ./data/tilesets/primary/cave_coronet ./tiles/cave_coronet ./include/constants/metatile_behaviors.h
