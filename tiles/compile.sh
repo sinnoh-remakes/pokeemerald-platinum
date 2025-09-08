051ce182e3900b110e39a4579b3bbc54c8e38f61
blloop <42186044+blloop@users.noreply.github.com>
2025-09-08 00:00:46 -0700

    add mountain tile, begin map transition areas

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "Compiling [secondary] jubilife"
-porytiles compile-secondary -Wall -o ./data/tilesets/secondary/jubilife ./tiles/jubilife ./tiles/outdoor_jubilife ./include/constants/metatile_behaviors.h
-echo "Compiling [PRIMARY] cave_coronet"
-porytiles compile-primary -Wall -o ./data/tilesets/primary/cave_coronet ./tiles/cave_coronet ./include/constants/metatile_behaviors.h
+echo "Compiling [PRIMARY] outdoor_oreburgh"
+porytiles compile-primary -Wall -o ./data/tilesets/primary/outdoor_oreburgh ./tiles/outdoor_oreburgh ./include/constants/metatile_behaviors.h
