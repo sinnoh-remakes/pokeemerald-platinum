c1b92406fb3196de5c4ee4a2d6bf0cefcb756bc5
blloop <42186044+blloop@users.noreply.github.com>
2025-10-26 15:21:34 -0700

    add vertical coal tile anims

--- src/tileset_anims.c
+++ src/tileset_anims.c
@@ -1322,3 +1322,43 @@ void InitTilesetAnim_Lake(void)
     sSecondaryTilesetAnimCounterMax = 256;
     sSecondaryTilesetAnimCallback = TilesetAnim_Lake;
 }
+
+const u16 gTilesetAnims_Coal_Vertical_Frame0[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/00.4bpp");
+const u16 gTilesetAnims_Coal_Vertical_Frame1[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/01.4bpp");
+const u16 gTilesetAnims_Coal_Vertical_Frame2[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/02.4bpp");
+const u16 gTilesetAnims_Coal_Vertical_Frame3[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/03.4bpp");
+const u16 gTilesetAnims_Coal_Vertical_Frame4[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/04.4bpp");
+const u16 gTilesetAnims_Coal_Vertical_Frame5[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/05.4bpp");
+const u16 gTilesetAnims_Coal_Vertical_Frame6[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/06.4bpp");
+const u16 gTilesetAnims_Coal_Vertical_Frame7[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/07.4bpp");
+
+const u16 *const gTilesetAnims_Coal_Vertical[] = {
+    gTilesetAnims_Coal_Vertical_Frame0,
+    gTilesetAnims_Coal_Vertical_Frame1,
+    gTilesetAnims_Coal_Vertical_Frame2,
+    gTilesetAnims_Coal_Vertical_Frame3,
+    gTilesetAnims_Coal_Vertical_Frame4,
+    gTilesetAnims_Coal_Vertical_Frame5,
+    gTilesetAnims_Coal_Vertical_Frame6,
+    gTilesetAnims_Coal_Vertical_Frame7
+};
+
+static void QueueAnimTiles_Coal_Vertical(u16 timer)
+{
+    u16 i = timer % ARRAY_COUNT(gTilesetAnims_Coal_Vertical);
+    AppendTilesetAnimToBuffer(gTilesetAnims_Coal_Vertical[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(512)), 4 * TILE_SIZE_4BPP);
+}
+
+static void TilesetAnim_Oreburgh(u16 timer)
+{
+    if (timer % 16 == 0) {
+        QueueAnimTiles_Coal_Vertical(timer / 16);
+    }
+}
+
+void InitTilesetAnim_Oreburgh(void)
+{
+    sSecondaryTilesetAnimCounter = 0;
+    sSecondaryTilesetAnimCounterMax = 256;
+    sSecondaryTilesetAnimCallback = TilesetAnim_Oreburgh;
+}
