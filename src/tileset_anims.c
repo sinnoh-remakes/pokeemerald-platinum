5a1a34bd73c53bdfb2ecbadf0634fbf78c775b39
blloop <42186044+blloop@users.noreply.github.com>
2025-11-01 23:21:29 -0700

    add horizontal coal tiles, rename anim set to orthogonal

--- src/tileset_anims.c
+++ src/tileset_anims.c
@@ -1323,36 +1323,36 @@ void InitTilesetAnim_Lake(void)
     sSecondaryTilesetAnimCallback = TilesetAnim_Lake;
 }
 
-const u16 gTilesetAnims_Coal_Vertical_Frame0[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/00.4bpp");
-const u16 gTilesetAnims_Coal_Vertical_Frame1[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/01.4bpp");
-const u16 gTilesetAnims_Coal_Vertical_Frame2[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/02.4bpp");
-const u16 gTilesetAnims_Coal_Vertical_Frame3[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/03.4bpp");
-const u16 gTilesetAnims_Coal_Vertical_Frame4[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/04.4bpp");
-const u16 gTilesetAnims_Coal_Vertical_Frame5[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/05.4bpp");
-const u16 gTilesetAnims_Coal_Vertical_Frame6[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/06.4bpp");
-const u16 gTilesetAnims_Coal_Vertical_Frame7[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_vertical/07.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame0[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/00.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame1[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/01.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame2[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/02.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame3[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/03.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame4[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/04.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame5[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/05.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame6[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/06.4bpp");
+const u16 gTilesetAnims_Coal_Orthogonal_Frame7[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_orthogonal/07.4bpp");
 
-const u16 *const gTilesetAnims_Coal_Vertical[] = {
-    gTilesetAnims_Coal_Vertical_Frame0,
-    gTilesetAnims_Coal_Vertical_Frame1,
-    gTilesetAnims_Coal_Vertical_Frame2,
-    gTilesetAnims_Coal_Vertical_Frame3,
-    gTilesetAnims_Coal_Vertical_Frame4,
-    gTilesetAnims_Coal_Vertical_Frame5,
-    gTilesetAnims_Coal_Vertical_Frame6,
-    gTilesetAnims_Coal_Vertical_Frame7
+const u16 *const gTilesetAnims_Coal_Orthogonal[] = {
+    gTilesetAnims_Coal_Orthogonal_Frame0,
+    gTilesetAnims_Coal_Orthogonal_Frame1,
+    gTilesetAnims_Coal_Orthogonal_Frame2,
+    gTilesetAnims_Coal_Orthogonal_Frame3,
+    gTilesetAnims_Coal_Orthogonal_Frame4,
+    gTilesetAnims_Coal_Orthogonal_Frame5,
+    gTilesetAnims_Coal_Orthogonal_Frame6,
+    gTilesetAnims_Coal_Orthogonal_Frame7
 };
 
-static void QueueAnimTiles_Coal_Vertical(u16 timer)
+static void QueueAnimTiles_Coal_Orthogonal(u16 timer)
 {
-    u16 i = timer % ARRAY_COUNT(gTilesetAnims_Coal_Vertical);
-    AppendTilesetAnimToBuffer(gTilesetAnims_Coal_Vertical[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(512)), 4 * TILE_SIZE_4BPP);
+    u16 i = timer % ARRAY_COUNT(gTilesetAnims_Coal_Orthogonal);
+    AppendTilesetAnimToBuffer(gTilesetAnims_Coal_Orthogonal[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(512)), 4 * TILE_SIZE_4BPP);
 }
 
 static void TilesetAnim_Oreburgh(u16 timer)
 {
     if (timer % 16 == 0) {
-        QueueAnimTiles_Coal_Vertical(timer / 16);
+        QueueAnimTiles_Coal_Orthogonal(timer / 16);
     }
 }
 
