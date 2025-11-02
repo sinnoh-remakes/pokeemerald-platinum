6078735f859d30b6446a781ce70c1bc2e11a040b
blloop <42186044+blloop@users.noreply.github.com>
2025-11-01 23:34:14 -0700

    add downward shallow slope coal tiles

--- src/tileset_anims.c
+++ src/tileset_anims.c
@@ -1346,13 +1346,42 @@ const u16 *const gTilesetAnims_Coal_Orthogonal[] = {
 static void QueueAnimTiles_Coal_Orthogonal(u16 timer)
 {
     u16 i = timer % ARRAY_COUNT(gTilesetAnims_Coal_Orthogonal);
-    AppendTilesetAnimToBuffer(gTilesetAnims_Coal_Orthogonal[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(512)), 4 * TILE_SIZE_4BPP);
+    AppendTilesetAnimToBuffer(gTilesetAnims_Coal_Orthogonal[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(512)), 8 * TILE_SIZE_4BPP);
+}
+
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame0[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/00.4bpp");
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame1[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/01.4bpp");
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame2[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/02.4bpp");
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame3[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/03.4bpp");
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame4[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/04.4bpp");
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame5[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/05.4bpp");
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame6[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/06.4bpp");
+const u16 gTilesetAnims_Coal_Slope_Shallow_Frame7[] = INCBIN_U16("data/tilesets/secondary/oreburgh_south/anim/coal_slope_shallow/07.4bpp");
+
+const u16 *const gTilesetAnims_Coal_Slope_Shallow[] = {
+    gTilesetAnims_Coal_Slope_Shallow_Frame0,
+    gTilesetAnims_Coal_Slope_Shallow_Frame1,
+    gTilesetAnims_Coal_Slope_Shallow_Frame2,
+    gTilesetAnims_Coal_Slope_Shallow_Frame3,
+    gTilesetAnims_Coal_Slope_Shallow_Frame4,
+    gTilesetAnims_Coal_Slope_Shallow_Frame5,
+    gTilesetAnims_Coal_Slope_Shallow_Frame6,
+    gTilesetAnims_Coal_Slope_Shallow_Frame7
+};
+
+static void QueueAnimTiles_Coal_Slope_Shallow(u16 timer)
+{
+    u16 i = timer % ARRAY_COUNT(gTilesetAnims_Coal_Slope_Shallow);
+    AppendTilesetAnimToBuffer(gTilesetAnims_Coal_Slope_Shallow[i], (u16 *)(BG_VRAM + TILE_OFFSET_4BPP(520)), 7 * TILE_SIZE_4BPP);
 }
 
 static void TilesetAnim_Oreburgh(u16 timer)
 {
     if (timer % 16 == 0) {
         QueueAnimTiles_Coal_Orthogonal(timer / 16);
+    }
+    if (timer % 16 == 1) {
+        QueueAnimTiles_Coal_Slope_Shallow(timer / 16);
     }
 }
 
