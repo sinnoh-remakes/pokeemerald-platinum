60637c84d5be1eee699e65277b904e2bacd31539
blloop <42186044+blloop@users.noreply.github.com>
2025-11-17 23:06:42 -0800

    add goods tiles to tileset

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -2,5 +2,5 @@
 
 echo "Compiling [PRIMARY] indoor_mart"
 porytiles compile-primary -Wall -o ./data/tilesets/primary/indoor_mart ./tiles/indoor_mart ./include/constants/metatile_behaviors.h
-# echo "Compiling [secondary] common_mart"
-# porytiles compile-secondary -Wall -o ./data/tilesets/secondary/common_mart ./tiles/common_mart ./tiles/indoor_mart ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] common_mart"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/common_mart ./tiles/common_mart ./tiles/indoor_mart ./include/constants/metatile_behaviors.h
