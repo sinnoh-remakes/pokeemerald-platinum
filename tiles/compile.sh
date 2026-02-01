687857af9d26bbff81d30c768bd5468a2726001b
Bill Yu <42186044+blloop@users.noreply.github.com>
2026-01-31 19:32:36 -0800

    Add Pokemon Center 2F Tileset and Layout (#22)
    This PR adds map layouts for 2F of the Pokemon Center. It also adds a
    corresponding secondary tileset `gTileset_CommonCenter2F` (secondary)
    and renames the existing tileset `gTileset_CommonCenter` to
    `gTilesetCommonCenter1F`
    
    <img width="503" height="201" alt="image"
    src="https://github.com/user-attachments/assets/2ef15adb-0549-4e41-8fc7-1172b689e0d8"
    />

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Compiling [PRIMARY] indoor_jubilife"
-porytiles compile-primary -Wall -o ./data/tilesets/primary/indoor_jubilife ./tiles/indoor_jubilife ./include/constants/metatile_behaviors.h
-echo "Compiling [secondary] jubilife_school"
-porytiles compile-secondary -Wall -o ./data/tilesets/secondary/jubilife_school ./tiles/jubilife_school ./tiles/indoor_jubilife ./include/constants/metatile_behaviors.h
+echo "Compiling [PRIMARY] indoor_center"
+porytiles compile-primary -Wall -o ./data/tilesets/primary/indoor_center ./tiles/indoor_center ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] common_center_1f"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/common_center_1f ./tiles/common_center_1f ./tiles/indoor_center ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] common_center_2f"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/common_center_2f ./tiles/common_center_2f ./tiles/indoor_center ./include/constants/metatile_behaviors.h
