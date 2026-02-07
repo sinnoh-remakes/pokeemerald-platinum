a750c63915f23e9f118ef479360153ffa857c296
blloop <42186044+blloop@users.noreply.github.com>
2026-02-07 14:27:43 -0800

    clean up flags

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,7 +43,7 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
+#define FLAG_ROUTE201_RECEIVED_POTION    0x20
 #define FLAG_UNUSED_0x021    0x21 // Unused Flag
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
@@ -98,7 +98,7 @@
 #define FLAG_RESCUED_BIRCH                       0x52
 #define FLAG_LEGENDARIES_IN_SOOTOPOLIS           0x53
 
-#define FLAG_ROUTE201_RECEIVED_POTION        0x54
+#define FLAG_UNUSED_0x054                    0x54  // Unused Flag
 #define FLAG_UNUSED_0x055                    0x55  // Unused Flag
 
 #define FLAG_HIDE_CONTEST_POKE_BALL          0x56  // Always set after new game, object it hides is added directly
@@ -1383,26 +1383,27 @@
 #define FLAG_VISITED_MOSSDEEP_CITY                  (SYSTEM_FLAGS + 0x1C)
 #define FLAG_VISITED_SOOTOPOLIS_CITY                (SYSTEM_FLAGS + 0x1D)
 #define FLAG_VISITED_EVER_GRANDE_CITY               (SYSTEM_FLAGS + 0x1E)
-#define FLAG_VISITED_TWINLEAF_TOWN                  (SYSTEM_FLAGS + 0x1F)
-#define FLAG_VISITED_SANDGEM_TOWN                   (SYSTEM_FLAGS + 0x20)
-#define FLAG_VISITED_JUBILIFE_CITY                  (SYSTEM_FLAGS + 0x21)
-#define FLAG_VISITED_OREBURGH_CITY                  (SYSTEM_FLAGS + 0x22)
 
-#define FLAG_UNUSED_0x883                           (SYSTEM_FLAGS + 0x23) // Unused Flag
-#define FLAG_UNUSED_0x884                           (SYSTEM_FLAGS + 0x24) // Unused Flag
+#define FLAG_IS_CHAMPION                            (SYSTEM_FLAGS + 0x1F) // Seems to be related to linking.
+#define FLAG_NURSE_UNION_ROOM_REMINDER              (SYSTEM_FLAGS + 0x20)
+
+#define FLAG_VISITED_TWINLEAF_TOWN                  (SYSTEM_FLAGS + 0x21)
+#define FLAG_VISITED_SANDGEM_TOWN                   (SYSTEM_FLAGS + 0x22)
+#define FLAG_VISITED_JUBILIFE_CITY                  (SYSTEM_FLAGS + 0x23)
+#define FLAG_VISITED_OREBURGH_CITY                  (SYSTEM_FLAGS + 0x24)
 #define FLAG_UNUSED_0x885                           (SYSTEM_FLAGS + 0x25) // Unused Flag
 #define FLAG_UNUSED_0x886                           (SYSTEM_FLAGS + 0x26) // Unused Flag
 #define FLAG_UNUSED_0x887                           (SYSTEM_FLAGS + 0x27) // Unused Flag
+#define FLAG_UNUSED_0x888                           (SYSTEM_FLAGS + 0x28) // Unused Flag
+#define FLAG_UNUSED_0x889                           (SYSTEM_FLAGS + 0x29) // Unused Flag
 
-#define FLAG_SYS_USE_FLASH                          (SYSTEM_FLAGS + 0x28)
-#define FLAG_SYS_USE_STRENGTH                       (SYSTEM_FLAGS + 0x29)
-#define FLAG_SYS_WEATHER_CTRL                       (SYSTEM_FLAGS + 0x2A)
-#define FLAG_SYS_CYCLING_ROAD                       (SYSTEM_FLAGS + 0x2B)
-#define FLAG_SYS_SAFARI_MODE                        (SYSTEM_FLAGS + 0x2C)
-#define FLAG_SYS_CRUISE_MODE                        (SYSTEM_FLAGS + 0x2D)
-
-#define FLAG_IS_CHAMPION                            (SYSTEM_FLAGS + 0x2E) // Seems to be related to linking.
-#define FLAG_NURSE_UNION_ROOM_REMINDER              (SYSTEM_FLAGS + 0x2F) // Unused Flag
+#define FLAG_SYS_USE_FLASH                          (SYSTEM_FLAGS + 0x2A)
+#define FLAG_SYS_USE_STRENGTH                       (SYSTEM_FLAGS + 0x2B)
+// Sets abnormal weather on maps that check for it
+#define FLAG_SYS_WEATHER_CTRL                       (SYSTEM_FLAGS + 0x2C)
+#define FLAG_SYS_CYCLING_ROAD                       (SYSTEM_FLAGS + 0x2D)
+#define FLAG_SYS_SAFARI_MODE                        (SYSTEM_FLAGS + 0x2E)
+#define FLAG_SYS_CRUISE_MODE                        (SYSTEM_FLAGS + 0x2F)
 
 #define FLAG_SYS_TV_HOME                            (SYSTEM_FLAGS + 0x30)
 #define FLAG_SYS_TV_WATCH                           (SYSTEM_FLAGS + 0x31)
