d9cce2c7f93cf3d66fa5358319b83aaa0f127bfb
blloop <42186044+blloop@users.noreply.github.com>
2026-02-08 23:43:42 -0800

    add clown quiz events

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -671,7 +671,7 @@
 #define FLAG_HIDE_ROUTE_202_HELPER             0x270
 #define FLAG_HIDE_JUBILIFE_LOOKER              0x271
 #define FLAG_HIDE_JUBILIFE_HELPER              0x272
-#define FLAG_UNUSED_0x273  0x273 // Unused Flag
+#define FLAG_HIDE_JUBILIFE_CLOWNS              0x273
 #define FLAG_UNUSED_0x274  0x274 // Unused Flag
 #define FLAG_UNUSED_0x275  0x275 // Unused Flag
 #define FLAG_UNUSED_0x276  0x276 // Unused Flag
