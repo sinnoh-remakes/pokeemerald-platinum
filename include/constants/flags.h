aef6c1c3d956e4a00de00f2f0e62b064e1ebc6e7
blloop <42186044+blloop@users.noreply.github.com>
2026-02-08 22:57:02 -0800

    add support for global terminal guard

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -44,7 +44,7 @@
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
 #define FLAG_ROUTE201_RECEIVED_POTION    0x20
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
+#define FLAG_GLOBAL_TERMINAL_OPEN        0x21
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
