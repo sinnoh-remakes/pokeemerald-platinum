96a7c54d4ce9f8221e2d21cd151e125905a60474
blloop <42186044+blloop@users.noreply.github.com>
2026-02-12 20:59:23 -0800

    track clown completion status with flags

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -45,9 +45,9 @@
 
 #define FLAG_ROUTE201_RECEIVED_POTION    0x20
 #define FLAG_GLOBAL_TERMINAL_OPEN        0x21
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
+#define FLAG_JUBILIFE_CLOWN_GROW         0x22
+#define FLAG_JUBILIFE_CLOWN_ITEM         0x23
+#define FLAG_JUBILIFE_CLOWN_TYPE         0x24
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
