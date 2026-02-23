959523be1e0f95c3fae4e491f1e40be1f6adab32
blloop <42186044+blloop@users.noreply.github.com>
2026-02-22 22:59:06 -0800

    add looker block event in east exit

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -670,10 +670,10 @@
 #define FLAG_HIDE_PLAYER_HOUSE_RIVAL_MOM       0x26F
 #define FLAG_HIDE_ROUTE_202_HELPER             0x270
 #define FLAG_HIDE_JUBILIFE_LOOKER              0x271
-#define FLAG_HIDE_JUBILIFE_HELPER              0x272
-#define FLAG_HIDE_JUBILIFE_POKETCH             0x273
-#define FLAG_HIDE_TRAINER_SCHOOL_RIVAL         0x274
-#define FLAG_UNUSED_0x275  0x275 // Unused Flag
+#define FLAG_HIDE_JUBILIFE_LOOKER_2            0x272
+#define FLAG_HIDE_JUBILIFE_HELPER              0x273
+#define FLAG_HIDE_JUBILIFE_POKETCH             0x274
+#define FLAG_HIDE_TRAINER_SCHOOL_RIVAL         0x275
 #define FLAG_UNUSED_0x276  0x276 // Unused Flag
 #define FLAG_UNUSED_0x277  0x277 // Unused Flag
 #define FLAG_UNUSED_0x278  0x278 // Unused Flag
