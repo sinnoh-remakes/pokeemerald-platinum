sinnoh-remakes/pokeemerald-platinum

commit 0eaa8b7458675286e1a02671fd89ad009cf4a249

Update NPC Sprites, Improve Starter Selection, Add Mesprit Cry (#41)
This PR updates the starter selection scene to more closely match what
is in Platinum, and adds in the Mesprit cry during the Lake Verity
script. It also update existing Sinnoh maps to use the new NPC sprites. 

## Description
This removes the birch rescue battle with Zigzagoon, changes the starter
selection scene by removing the labels that show the Pokemon name/"type"
when hovering over a pokeball, makes it so when the left starter preview
is selected the name and "type" is (e.g. "Tiny Leaf Pokemon Turtwig") is
shown in the message box, reflects the correct color corresponding to
the type trio, and has a different message for each of the three starter
options similar to Platinum. This PR also adds in the cry for Mesprit
during the initial Lake Verity script.

## Media
<img width="1470" height="956" alt="Screenshot 2026-03-23 at 05 48 32"
src="https://github.com/user-attachments/assets/0d6455de-fa19-46c0-ab7a-b0e5178e481b"
/>
<img width="1470" height="956" alt="Screenshot 2026-03-23 at 05 48 39"
src="https://github.com/user-attachments/assets/05536db2-fa68-4ceb-b48e-2e3ec7b338bc"
/>
<img width="1470" height="956" alt="Screenshot 2026-03-23 at 05 48 53"
src="https://github.com/user-attachments/assets/be50b5b6-c3e0-4099-a86b-b9599999eea4"
/>

## CREDITS
Aeonyx3 for the "green flash" fix after the starter selection screen.

## Feature(s) this PR does NOT handle:
- This PR does not change the graphics for the starter selection screen
to match Platinum and uses the existing graphics from Emerald.
- This PR also does not match the color of the shadowing of the
lettering of the name/"type" in the confirmation portion of selecting
each starter to it's corresponding lighter shade of R/G/B.


## Discord contact info
Willic5783

src/battle_controllers.c
@@ -66,34 +66,34 @@ void HandleLinkBattleSetup(void)
     }
 }
 
-void SetUpBattleVarsAndBirchZigzagoon(void)
-{
-    s32 i;
-
-    gBattleMainFunc = BeginBattleIntroDummy;
-
-    for (i = 0; i < MAX_BATTLERS_COUNT; i++)
-    {
-        gBattlerControllerFuncs[i] = BattleControllerDummy;
-        gBattlerPositions[i] = 0xFF;
-        gActionSelectionCursor[i] = 0;
-        gMoveSelectionCursor[i] = 0;
-    }
-
-    HandleLinkBattleSetup();
-    gBattleControllerExecFlags = 0;
-    ClearBattleAnimationVars();
-    BattleAI_SetupItems();
-    BattleAI_SetupFlags();
-
-    if (gBattleTypeFlags & BATTLE_TYPE_FIRST_BATTLE)
-    {
-        ZeroEnemyPartyMons();
-        CreateMon(&gEnemyParty[0], SPECIES_ZIGZAGOON, 2, USE_RANDOM_IVS, 0, 0, OT_ID_PLAYER_ID, 0);
-        i = 0;
-        SetMonData(&gEnemyParty[0], MON_DATA_HELD_ITEM, &i);
-    }
-}
+// void SetUpBattleVarsAndBirchZigzagoon(void)
+// {
+//     s32 i;
+//
+//     gBattleMainFunc = BeginBattleIntroDummy;
+//
+//     for (i = 0; i < MAX_BATTLERS_COUNT; i++)
+//     {
+//         gBattlerControllerFuncs[i] = BattleControllerDummy;
+//         gBattlerPositions[i] = 0xFF;
+//         gActionSelectionCursor[i] = 0;
+//         gMoveSelectionCursor[i] = 0;
+//     }
+//
+//     HandleLinkBattleSetup();
+//     gBattleControllerExecFlags = 0;
+//     ClearBattleAnimationVars();
+//     BattleAI_SetupItems();
+//     BattleAI_SetupFlags();
+//
+//     if (gBattleTypeFlags & BATTLE_TYPE_FIRST_BATTLE)
+//     {
+    //     ZeroEnemyPartyMons();
+    //     CreateMon(&gEnemyParty[0], SPECIES_ZIGZAGOON, 2, USE_RANDOM_IVS, 0, 0, OT_ID_PLAYER_ID, 0);
+    //     i = 0;
+    //     SetMonData(&gEnemyParty[0], MON_DATA_HELD_ITEM, &i);
+//     }
+// }
 
 void InitBattleControllers(void)
 {

src/battle_main.c
@@ -548,7 +548,7 @@ static void CB2_InitBattleInternal(void)
     FreeAllSpritePalettes();
     gReservedSpritePaletteCount = MAX_BATTLERS_COUNT;
     SetVBlankCallback(VBlankCB_Battle);
-    SetUpBattleVarsAndBirchZigzagoon();
+    // SetUpBattleVarsAndBirchZigzagoon();
 
     if ((IsMultibattleTest() && gBattleTypeFlags & BATTLE_TYPE_INGAME_PARTNER)
     || (gBattleTypeFlags & BATTLE_TYPE_MULTI && gBattleTypeFlags & BATTLE_TYPE_BATTLE_TOWER)

src/battle_setup.c
@@ -70,7 +70,10 @@ static void CB2_EndWildBattle(void);
 static void CB2_EndScriptedWildBattle(void);
 static void TryUpdateGymLeaderRematchFromWild(void);
 static void TryUpdateGymLeaderRematchFromTrainer(void);
-static void CB2_GiveStarter(void);
+// static void CB2_GiveStarter(void);
+static void CB2_GiveStarterNoBattle(void);
+// static void CB2_StartFirstBattle(void);
+// static void CB2_EndFirstBattle(void);
 static void SaveChangesToPlayerParty(void);
 static void HandleBattleVariantEndParty(void);
 static void CB2_EndTrainerBattle(void);
@@ -871,21 +874,63 @@ enum BattleTransition GetSpecialBattleTransition(enum BattleTransitionGroup id)
 void ChooseStarter(void)
 {
     SetMainCallback2(CB2_ChooseStarter);
-    gMain.savedCallback = CB2_GiveStarter;
+    gMain.savedCallback = CB2_GiveStarterNoBattle;
 }
 
-static void CB2_GiveStarter(void)
+// static void CB2_GiveStarter(void)
+// {
+//     u16 starterMon;
+//
+//     *GetVarPointer(VAR_STARTER_MON) = gSpecialVar_Result;
+//     starterMon = GetStarterPokemon(gSpecialVar_Result);
+//     ScriptGiveMon(starterMon, 5, ITEM_NONE);
+//     ResetTasks();
+//     PlayBattleBGM();
+//     SetMainCallback2(CB2_StartFirstBattle);
+//     BattleTransition_Start(B_TRANSITION_BLUR);
+// }
+
+static void CB2_GiveStarterNoBattle(void)
 {
+    UpdatePaletteFade();
     u16 starterMon;
 
     *GetVarPointer(VAR_STARTER_MON) = gSpecialVar_Result;
     starterMon = GetStarterPokemon(gSpecialVar_Result);
     ScriptGiveMon(starterMon, 5, ITEM_NONE);
     ResetTasks();
-    Overworld_ClearSavedMusic();
-    SetMainCallback2(CB2_ReturnToFieldContinueScriptPlayMapMusic);
+    // PlayBattleBGM();
+    SetMainCallback2(CB2_ReturnToFieldFadeFromBlack);
+    // BattleTransition_Start(B_TRANSITION_BLUR);
 }
 
+// static void CB2_StartFirstBattle(void)
+// {
+//     UpdatePaletteFade();
+//     RunTasks();
+//
+//     if (IsBattleTransitionDone() == TRUE)
+//     {
+//         gBattleTypeFlags = BATTLE_TYPE_FIRST_BATTLE;
+//         gMain.savedCallback = CB2_EndFirstBattle;
+//         FreeAllWindowBuffers();
+//         SetMainCallback2(CB2_InitBattle);
+//         RestartWildEncounterImmunitySteps();
+//         ClearPoisonStepCounter();
+//         IncrementGameStat(GAME_STAT_TOTAL_BATTLES);
+//         IncrementGameStat(GAME_STAT_WILD_BATTLES);
+//         IncrementDailyWildBattles();
+//         TryUpdateGymLeaderRematchFromWild();
+//     }
+// }
+//
+// static void CB2_EndFirstBattle(void)
+// {
+//     Overworld_ClearSavedMusic();
+//     DowngradeBadPoison();
+//     SetMainCallback2(CB2_ReturnToFieldContinueScriptPlayMapMusic);
+// }
+//
 static void TryUpdateGymLeaderRematchFromWild(void)
 {
     if (GetGameStat(GAME_STAT_WILD_BATTLES) % 60 == 0)

data/maps/LakeVerity/scripts.inc
@@ -57,8 +57,9 @@ LakeVerity_EventScript_IntroScene::
 	waitmovement 0
 	delay 48
 	msgbox LakeVerity_Text_LetsCatch, MSGBOX_DEFAULT
-	@ FODO: Play mesprit sound
+	playmoncry SPECIES_MESPRIT, CRY_MODE_ENCOUNTER
 	msgbox LakeVerity_Text_Kyaun, MSGBOX_SIGN
+	waitmoncry
 	playse SE_PIN
 	applymovement LOCALID_LAKE_VERITY_RIVAL, Common_Movement_ExclamationMark
 	waitmovement 0

include/strings.h
@@ -264,7 +264,11 @@ extern const u8 gText_Birch_YourePlayer[];
 extern const u8 gText_Birch_AreYouReady[];
 
 extern const u8 gText_BirchInTrouble[];
+extern const u8 gText_RowanGiveStarter[];
 extern const u8 gText_ConfirmStarterChoice[];
+extern const u8 gText_ConfirmStarterChoiceLT[];
+extern const u8 gText_ConfirmStarterChoiceMD[];
+extern const u8 gText_ConfirmStarterChoiceRT[];
 
 // mystery event menu text
 extern const u8 gText_EventSafelyLoaded[];

src/starter_choose.c
@@ -15,6 +15,7 @@
 #include "sprite.h"
 #include "starter_choose.h"
 #include "strings.h"
+#include "string_util.h"
 #include "task.h"
 #include "text.h"
 #include "text_window.h"
@@ -42,7 +43,7 @@ static void Task_AskConfirmStarter(u8 taskId);
 static void Task_HandleConfirmStarterInput(u8 taskId);
 static void Task_DeclineStarter(u8 taskId);
 static void Task_MoveStarterChooseCursor(u8 taskId);
-static void Task_CreateStarterLabel(u8 taskId);
+// static void Task_CreateStarterLabel(u8 taskId);
 static void CreateStarterPokemonLabel(u8 selection);
 static u8 CreatePokemonFrontSprite(u16 species, u8 x, u8 y);
 static void SpriteCB_SelectionHand(struct Sprite *sprite);
@@ -148,7 +149,7 @@ static const struct BgTemplate sBgTemplates[3] =
     },
 };
 
-static const u8 sTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_WHITE, TEXT_COLOR_LIGHT_GRAY};
+static const u8 sTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_WHITE, TEXT_COLOR_LIGHT_GRAY, TEXT_COLOR_GREEN, TEXT_COLOR_RED, TEXT_COLOR_BLUE};
 
 static const struct OamData sOam_Hand =
 {
@@ -459,7 +460,7 @@ void CB2_ChooseStarter(void)
     gSprites[spriteId].sTaskId = taskId;
     gSprites[spriteId].sBallId = 2;
 
-    sStarterLabelWindowId = WINDOW_NONE;
+    // sStarterLabelWindowId = WINDOW_NONE;
 }
 
 static void CB2_StarterChoose(void)
@@ -475,7 +476,7 @@ static void Task_StarterChoose(u8 taskId)
 {
     CreateStarterPokemonLabel(gTasks[taskId].tStarterSelection);
     DrawStdFrameWithCustomTileAndPalette(0, FALSE, 0x2A8, 0xD);
-    AddTextPrinterParameterized(0, FONT_NORMAL, gText_BirchInTrouble, 0, 1, 0, NULL);
+    AddTextPrinterParameterized(0, FONT_NORMAL, gText_RowanGiveStarter, 0, 1, 0, NULL);
     PutWindowTilemap(0);
     ScheduleBgCopyTilemapToVram(0);
     gTasks[taskId].func = Task_HandleStarterChooseInput;
@@ -527,9 +528,33 @@ static void Task_WaitForStarterSprite(u8 taskId)
 
 static void Task_AskConfirmStarter(u8 taskId)
 {
+    u8 categoryText[32];
+    const u8 *speciesName;
+    u8 selection = gTasks[taskId].tStarterSelection;
+    u16 species = GetStarterPokemon(selection);
+
+    CopyMonCategoryText(species, categoryText);
+    speciesName = GetSpeciesName(species);
+
+    StringCopy(gStringVar1, speciesName);
+    StringCopy(gStringVar2, categoryText);
+
     PlayCry_Normal(GetStarterPokemon(gTasks[taskId].tStarterSelection), 0);
     FillWindowPixelBuffer(0, PIXEL_FILL(1));
-    AddTextPrinterParameterized(0, FONT_NORMAL, gText_ConfirmStarterChoice, 0, 1, 0, NULL);
+
+    static const u8 *const sStarterConfirmStrings[] =
+    {
+        gText_ConfirmStarterChoiceLT,
+        gText_ConfirmStarterChoiceMD,
+        gText_ConfirmStarterChoiceRT
+    };
+
+    if (selection > 2)
+        selection = 0;
+
+    StringExpandPlaceholders(gStringVar4, sStarterConfirmStrings[selection]);
+    AddTextPrinterParameterized(0, 1, gStringVar4, 0, 1, 0, NULL);
+
     ScheduleBgCopyTilemapToVram(0);
     CreateYesNoMenu(&sWindowTemplate_ConfirmStarter, 0x2A8, 0xD, 0);
     gTasks[taskId].func = Task_HandleConfirmStarterInput;
@@ -546,6 +571,14 @@ static void Task_HandleConfirmStarterInput(u8 taskId)
         gSpecialVar_Result = gTasks[taskId].tStarterSelection;
         ResetAllPicSprites();
         BeginNormalPaletteFade(PALETTES_ALL, 0, 0x10, 0, RGB_BLACK);
+
+        spriteId = gTasks[taskId].tPkmnSpriteId;
+        FreeOamMatrix(gSprites[spriteId].oam.matrixNum);
+        DestroySprite(&gSprites[spriteId]);
+
+        spriteId = gTasks[taskId].tCircleSpriteId;
+        FreeOamMatrix(gSprites[spriteId].oam.matrixNum);
+        DestroySprite(&gSprites[spriteId]);
         SetMainCallback2(gMain.savedCallback);
         break;
     case 1:  // NO
@@ -574,15 +607,15 @@ static void CreateStarterPokemonLabel(u8 selection)
     struct WindowTemplate winTemplate;
     const u8 *speciesName;
     s32 width;
-    u8 labelLeft, labelRight, labelTop, labelBottom;
+    // u8 labelLeft, labelRight, labelTop, labelBottom;
 
     u16 species = GetStarterPokemon(selection);
     CopyMonCategoryText(species, categoryText);
     speciesName = GetSpeciesName(species);
 
-    winTemplate = sWindowTemplate_StarterLabel;
-    winTemplate.tilemapLeft = sStarterLabelCoords[selection][0];
-    winTemplate.tilemapTop = sStarterLabelCoords[selection][1];
+    // winTemplate = sWindowTemplate_StarterLabel;
+    // winTemplate.tilemapLeft = sStarterLabelCoords[selection][0];
+    // winTemplate.tilemapTop = sStarterLabelCoords[selection][1];
 
     sStarterLabelWindowId = AddWindow(&winTemplate);
     FillWindowPixelBuffer(sStarterLabelWindowId, PIXEL_FILL(0));
@@ -596,12 +629,12 @@ static void CreateStarterPokemonLabel(u8 selection)
     PutWindowTilemap(sStarterLabelWindowId);
     ScheduleBgCopyTilemapToVram(0);
 
-    labelLeft = sStarterLabelCoords[selection][0] * 8 - 4;
-    labelRight = (sStarterLabelCoords[selection][0] + 13) * 8 + 4;
-    labelTop = sStarterLabelCoords[selection][1] * 8;
-    labelBottom = (sStarterLabelCoords[selection][1] + 4) * 8;
-    SetGpuReg(REG_OFFSET_WIN0H, WIN_RANGE(labelLeft, labelRight));
-    SetGpuReg(REG_OFFSET_WIN0V, WIN_RANGE(labelTop, labelBottom));
+    // labelLeft = sStarterLabelCoords[selection][0] * 8 - 4;
+    // labelRight = (sStarterLabelCoords[selection][0] + 13) * 8 + 4;
+    // labelTop = sStarterLabelCoords[selection][1] * 8;
+    // labelBottom = (sStarterLabelCoords[selection][1] + 4) * 8;
+    // SetGpuReg(REG_OFFSET_WIN0H, WIN_RANGE(labelLeft, labelRight));
+    // SetGpuReg(REG_OFFSET_WIN0V, WIN_RANGE(labelTop, labelBottom));
 }
 
 static void ClearStarterLabel(void)
@@ -618,12 +651,13 @@ static void ClearStarterLabel(void)
 static void Task_MoveStarterChooseCursor(u8 taskId)
 {
     ClearStarterLabel();
-    gTasks[taskId].func = Task_CreateStarterLabel;
+    // gTasks[taskId].func = Task_CreateStarterLabel;
+    gTasks[taskId].func = Task_HandleStarterChooseInput;
 }
 
 static void Task_CreateStarterLabel(u8 taskId)
 {
-    CreateStarterPokemonLabel(gTasks[taskId].tStarterSelection);
+    // CreateStarterPokemonLabel(gTasks[taskId].tStarterSelection);
     gTasks[taskId].func = Task_HandleStarterChooseInput;
 }
 

src/strings.c
@@ -119,7 +119,11 @@ const u8 gText_IdNumberSlash[] = _("IDNo. /"); // Unused
 const u8 gText_Name[] = _("NAME");
 const u8 gText_IDNumber[] = _("IDNo.");
 const u8 gText_BirchInTrouble[] = _("PROF. BIRCH is in trouble!\nRelease a POKéMON and rescue him!");
-const u8 gText_ConfirmStarterChoice[] = _("Do you choose this POKéMON?");
+const u8 gText_RowanGiveStarter[] = _("Now choose!\nWhich Pokémon will it be?");
+const u8 gText_ConfirmStarterChoice[] = _("INSERT COLORED MON NAME HERE!\nDo you choose this Pokémon?");
+const u8 gText_ConfirmStarterChoiceLT[] = _("{COLOR 05}{STR_VAR_2} {STR_VAR_1}{COLOR 02}!\nWill you take this Pokémon?");
+const u8 gText_ConfirmStarterChoiceMD[] = _("{COLOR 04}{STR_VAR_2} {STR_VAR_1}{COLOR 02}!\nDo you choose this Pokémon?");
+const u8 gText_ConfirmStarterChoiceRT[] = _("{COLOR 08}{STR_VAR_2} {STR_VAR_1}{COLOR 02}!\nIs this the Pokémon for you?");
 const u8 gText_Pokemon4[] = _("POKéMON"); // Unused
 const u8 gText_FlyToWhere[] = _("FLY to where?");
 const u8 gMenuText_Use[] = _("USE");

commit 7072d93745922c122083d67185b0ba45668dfcd6

Fix Broken Battle Initialization (#46)
This PR fixes battle engine issues introduced in #41. Specifically, the
commented out code removing the initial zigzagoon battle included
important battle initiation calls (setting the battler position,
initializing controller functions, setting up AI, etc.).

src/battle_controllers.c
@@ -66,34 +66,34 @@ void HandleLinkBattleSetup(void)
     }
 }
 
-// void SetUpBattleVarsAndBirchZigzagoon(void)
-// {
-//     s32 i;
-//
-//     gBattleMainFunc = BeginBattleIntroDummy;
-//
-//     for (i = 0; i < MAX_BATTLERS_COUNT; i++)
-//     {
-//         gBattlerControllerFuncs[i] = BattleControllerDummy;
-//         gBattlerPositions[i] = 0xFF;
-//         gActionSelectionCursor[i] = 0;
-//         gMoveSelectionCursor[i] = 0;
-//     }
-//
-//     HandleLinkBattleSetup();
-//     gBattleControllerExecFlags = 0;
-//     ClearBattleAnimationVars();
-//     BattleAI_SetupItems();
-//     BattleAI_SetupFlags();
-//
-//     if (gBattleTypeFlags & BATTLE_TYPE_FIRST_BATTLE)
-//     {
+void SetUpBattleVarsAndBirchZigzagoon(void)
+{
+    s32 i;
+
+    gBattleMainFunc = BeginBattleIntroDummy;
+
+    for (i = 0; i < MAX_BATTLERS_COUNT; i++)
+    {
+        gBattlerControllerFuncs[i] = BattleControllerDummy;
+        gBattlerPositions[i] = 0xFF;
+        gActionSelectionCursor[i] = 0;
+        gMoveSelectionCursor[i] = 0;
+    }
+
+    HandleLinkBattleSetup();
+    gBattleControllerExecFlags = 0;
+    ClearBattleAnimationVars();
+    BattleAI_SetupItems();
+    BattleAI_SetupFlags();
+
+    // if (gBattleTypeFlags & BATTLE_TYPE_FIRST_BATTLE)
+    // {
     //     ZeroEnemyPartyMons();
     //     CreateMon(&gEnemyParty[0], SPECIES_ZIGZAGOON, 2, USE_RANDOM_IVS, 0, 0, OT_ID_PLAYER_ID, 0);
     //     i = 0;
     //     SetMonData(&gEnemyParty[0], MON_DATA_HELD_ITEM, &i);
-//     }
-// }
+    // }
+}
 
 void InitBattleControllers(void)
 {

src/battle_main.c
@@ -548,7 +548,7 @@ static void CB2_InitBattleInternal(void)
     FreeAllSpritePalettes();
     gReservedSpritePaletteCount = MAX_BATTLERS_COUNT;
     SetVBlankCallback(VBlankCB_Battle);
-    // SetUpBattleVarsAndBirchZigzagoon();
+    SetUpBattleVarsAndBirchZigzagoon();
 
     if ((IsMultibattleTest() && gBattleTypeFlags & BATTLE_TYPE_INGAME_PARTNER)
     || (gBattleTypeFlags & BATTLE_TYPE_MULTI && gBattleTypeFlags & BATTLE_TYPE_BATTLE_TOWER)

src/battle_setup.c
@@ -70,8 +70,7 @@ static void CB2_EndWildBattle(void);
 static void CB2_EndScriptedWildBattle(void);
 static void TryUpdateGymLeaderRematchFromWild(void);
 static void TryUpdateGymLeaderRematchFromTrainer(void);
-// static void CB2_GiveStarter(void);
-static void CB2_GiveStarterNoBattle(void);
+static void CB2_GiveStarter(void);
 // static void CB2_StartFirstBattle(void);
 // static void CB2_EndFirstBattle(void);
 static void SaveChangesToPlayerParty(void);
@@ -874,23 +873,10 @@ enum BattleTransition GetSpecialBattleTransition(enum BattleTransitionGroup id)
 void ChooseStarter(void)
 {
     SetMainCallback2(CB2_ChooseStarter);
-    gMain.savedCallback = CB2_GiveStarterNoBattle;
+    gMain.savedCallback = CB2_GiveStarter;
 }
 
-// static void CB2_GiveStarter(void)
-// {
-//     u16 starterMon;
-//
-//     *GetVarPointer(VAR_STARTER_MON) = gSpecialVar_Result;
-//     starterMon = GetStarterPokemon(gSpecialVar_Result);
-//     ScriptGiveMon(starterMon, 5, ITEM_NONE);
-//     ResetTasks();
-//     PlayBattleBGM();
-//     SetMainCallback2(CB2_StartFirstBattle);
-//     BattleTransition_Start(B_TRANSITION_BLUR);
-// }
-
-static void CB2_GiveStarterNoBattle(void)
+static void CB2_GiveStarter(void)
 {
     UpdatePaletteFade();
     u16 starterMon;