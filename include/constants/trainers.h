c03ae85593512adc1cc8dad9ceceb0d2bdd6fd89
blloop <42186044+blloop@users.noreply.github.com>
2026-05-19 11:35:44 -0700

    remove unused sinnoh player facility classes

--- include/constants/trainers.h
+++ include/constants/trainers.h
@@ -301,13 +301,9 @@
 #define FACILITY_CLASS_RS_MAY                0x51
 #define FACILITY_CLASS_BARRY                 0x52
 #define FACILITY_CLASS_LUCAS                 0x53
-#define FACILITY_CLASS_LUCAS_2               0x54
-#define FACILITY_CLASS_LUCAS_3               0x55
-#define FACILITY_CLASS_DAWN                  0x56
-#define FACILITY_CLASS_DAWN_2                0x57
-#define FACILITY_CLASS_DAWN_3                0x58
+#define FACILITY_CLASS_DAWN                  0x54
 
-#define FACILITY_CLASSES_COUNT               0x59
+#define FACILITY_CLASSES_COUNT               0x55
 
 #define RS_FACILITY_CLASS_AQUA_LEADER_ARCHIE    0x0
 #define RS_FACILITY_CLASS_AQUA_GRUNT_M          0x1
