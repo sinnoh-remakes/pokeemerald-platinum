258457d0f4733862eb5c0ae04b7397e10707c2a8
blloop <42186044+blloop@users.noreply.github.com>
2026-06-10 16:33:27 -0700

    change intro music to MUS_DP_ROWAN

--- src/main_menu.c
+++ src/main_menu.c
@@ -1396,7 +1396,7 @@ static void Task_NewGameBirchSpeech_Init(u8 taskId)
     gTasks[taskId].tPlayerSpriteId = SPRITE_NONE;
     gTasks[taskId].data[3] = 0xFF;
     gTasks[taskId].tTimer = 0;
-    PlayBGM(MUS_ROUTE122);
+    PlayBGM(MUS_DP_ROWAN);
     ShowBg(0);
     ShowBg(1);
 }
