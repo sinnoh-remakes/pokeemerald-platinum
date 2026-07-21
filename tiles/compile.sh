0f54c823a0ed8558cf6c442ec8bd7afb03d99af8
blloop <42186044+blloop@users.noreply.github.com>
2026-07-20 22:35:34 -0700

    add floaroma town extension for smooth transition

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "Compiling [PRIMARY] outdoor_route_205"
-porytiles compile-primary -Wall -o ./data/tilesets/primary/outdoor_route_205 ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
 echo "Compiling [secondary] route_205_south"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/route_205_south ./tiles/route_205_south ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
