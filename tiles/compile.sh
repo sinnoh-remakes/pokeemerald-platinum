628033e6fbd84a05d32cfac535e552858cb2ff38
blloop <42186044+blloop@users.noreply.github.com>
2026-07-24 22:47:20 -0700

    refactor half mountain tiles

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,4 +1,8 @@
-echo "Compiling [PRIMARY] outdoor_route_204"
-porytiles compile-primary -Wall -o ./data/tilesets/primary/outdoor_route_204 ./tiles/outdoor_route_204 ./include/constants/metatile_behaviors.h
-echo "Compiling [secondary] route_204_north"
-porytiles compile-secondary -Wall -o ./data/tilesets/secondary/route_204_north ./tiles/route_204_north ./tiles/outdoor_route_204 ./include/constants/metatile_behaviors.h
+#!/bin/bash
+
+echo "Compiling [PRIMARY] outdoor_route_205"
+porytiles compile-primary -Wall -o ./data/tilesets/primary/outdoor_route_205 ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] route_205_south_1"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/route_205_south_1 ./tiles/route_205_south_1 ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] route_205_south_2"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/route_205_south_2 ./tiles/route_205_south_2 ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
