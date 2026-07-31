2847bc02583d3117b7342bcf35f9fd332a6477f7
blloop <42186044+blloop@users.noreply.github.com>
2026-07-30 17:22:14 -0700

    add initial windworks tileset with honey tree and fene

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -6,3 +6,5 @@ echo "Compiling [secondary] route_205_south_1"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/route_205_south_1 ./tiles/route_205_south_1 ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
 echo "Compiling [secondary] route_205_south_2"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/route_205_south_2 ./tiles/route_205_south_2 ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] valley_windworks"
+porytiles compile-secondary -Wall -o ./data/tilesets/secondary/valley_windworks ./tiles/valley_windworks ./tiles/outdoor_route_205 ./include/constants/metatile_behaviors.h
