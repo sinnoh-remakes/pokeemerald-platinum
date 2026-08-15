8e5780a8d3fdc76145b4193d3273728ca6b4791b
blloop <42186044+blloop@users.noreply.github.com>
2026-08-15 09:29:47 -0700

    add foundation for eterna forest primary tileset

--- tiles/compile_all.sh
+++ tiles/compile_all.sh
@@ -112,3 +112,6 @@ echo "Compiling [PRIMARY] indoor_windworks"
 porytiles compile-primary -Wall -o ./data/tilesets/primary/indoor_windworks ./tiles/indoor_windworks ./include/constants/metatile_behaviors.h
 echo "Compiling [secondary] valley_windworks_indoor"
 porytiles compile-secondary -Wall -o ./data/tilesets/secondary/valley_windworks_indoor ./tiles/valley_windworks_indoor ./tiles/indoor_windworks ./include/constants/metatile_behaviors.h
+
+echo "Compiling [PRIMARY] indoor_forest"
+porytiles compile-primary -Wall -o ./data/tilesets/primary/indoor_forest ./tiles/indoor_forest ./include/constants/metatile_behaviors.h
