895b84128a322b6a33a15b98095562421dbcb59d
Bill Yu <42186044+blloop@users.noreply.github.com>
2026-08-19 16:36:55 -0700

    Implement Instant Honey Tree (#181)
    This PR resolves #159 by adding honey tree behavior that is triggered
    immediately once the player successfully interacts with the tree.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1233,3 +1233,4 @@ EventScript_VsSeekerChargingDone::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+	.include "data/scripts/honey_tree.inc"
