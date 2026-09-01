854283be1a6696e5411ec63117a0999a1c39e32b
blloop <42186044+blloop@users.noreply.github.com>
2026-09-01 13:40:45 -0700

    add galctic commander and boss music

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1481,6 +1481,12 @@ void PlayTrainerEncounterMusic(void)
         case TRAINER_ENCOUNTER_MUSIC_GALACTIC_GRUNT_F_DP:
             music = MUS_DP_ENCOUNTER_GALACTIC;
             break;
+        case TRAINER_ENCOUNTER_MUSIC_COMMANDER_DP:
+            music = MUS_DP_VS_GALACTIC_COMMANDER;
+            break;
+        case TRAINER_ENCOUNTER_MUSIC_BOSS_DP:
+            music = MUS_DP_VS_GALACTIC_BOSS;
+            break;
         case TRAINER_ENCOUNTER_MUSIC_AROMA_LADY_DP:
         case TRAINER_ENCOUNTER_MUSIC_BEAUTY_DP:
         case TRAINER_ENCOUNTER_MUSIC_BREEDER_F_DP:
