00f49b7bbe5b694cfffab5dbdee8f5dd15c8399c
blloop <42186044+blloop@users.noreply.github.com>
2026-09-17 19:56:44 -0700

    add stair and cave entrance tiles

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-echo "Compiling [PRIMARY] outdoor_eterna"
-porytiles compile-primary -tiles-primary-override=384 -Wall -o ./data/tilesets/primary/outdoor_eterna ./tiles/outdoor_eterna ./include/constants/metatile_behaviors.h
-echo "Compiling [secondary] eterna_north"
-porytiles compile-secondary -tiles-primary-override=384 -Wall -o ./data/tilesets/secondary/eterna_north ./tiles/eterna_north ./tiles/outdoor_eterna ./include/constants/metatile_behaviors.h
-echo "Compiling [secondary] eterna_south"
-porytiles compile-secondary -tiles-primary-override=384 -Wall -o ./data/tilesets/secondary/eterna_south ./tiles/eterna_south ./tiles/outdoor_eterna ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] route_211_west"
+porytiles compile-secondary -tiles-primary-override=384 -Wall -o ./data/tilesets/secondary/route_211_west ./tiles/route_211_west ./tiles/outdoor_eterna ./include/constants/metatile_behaviors.h
