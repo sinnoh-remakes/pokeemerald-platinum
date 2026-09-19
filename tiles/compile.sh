1eded3a268916819c2765d6b970b91520fb92266
blloop <42186044+blloop@users.noreply.github.com>
2026-09-18 17:50:09 -0700

    rename coronet tileset to south

--- tiles/compile.sh
+++ tiles/compile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "Compiling [secondary] route_211_west"
-porytiles compile-secondary -tiles-primary-override=384 -Wall -o ./data/tilesets/secondary/route_211_west ./tiles/route_211_west ./tiles/outdoor_eterna ./include/constants/metatile_behaviors.h
+echo "Compiling [PRIMARY] cave_coronet_south"
+porytiles compile-primary -tiles-primary-override=384 -Wall -o ./data/tilesets/primary/cave_coronet_south ./tiles/cave_coronet_south ./include/constants/metatile_behaviors.h
+echo "Compiling [secondary] ravaged_path"
+porytiles compile-secondary -tiles-primary-override=384 -Wall -o ./data/tilesets/secondary/ravaged_path ./tiles/ravaged_path ./tiles/cave_coronet_south ./include/constants/metatile_behaviors.h
